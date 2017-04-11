63269cef5ebd91060f626fcb4a1c108486f091da
Fredrik Håkansson <freha309@student.liu.se>
2017-04-11 12:07:17 +0200

    Display playlists in front end upon search

---
+++ components/PlaylistResult.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Fredrik on 2017-04-11.
+ */
+import React from 'react';
+
+export default class PlaylistResult extends React.component {
+	img_url = (function () {
+		let images = this.props.images;
+		for (let i = 0; i < images.length; i++) {
+			if (images[i].height == 300)
+				return images[i];
+		}
+	})();
+
+
+
+	render() {
+
+		return (
+			<div className="media">
+				<div className="media-left">
+					<a href={this.props.external_urls.spotify}>
+						<img className="media-object" src={this.img_url} alt={this.props.name} />
+					</a>
+				</div>
+				<div className="media-body">
+					<h4 className="media-heading">{this.props.name}</h4>
+					<p>Owner: {this.props.owner.id}, Tracks: {this.props.tracks.total}</p>
+				</div>
+			</div>
+		)
+	}
+}
--- components/SearchField.js
+++ components/SearchField.js
@@ -4,45 +4,71 @@
 import React from 'react';
 import Select from 'react-select';
 import fetch from 'isomorphic-fetch';
+import PlaylistResult from './PlaylistResult';
+
+var playlists = [];
 
 var SearchField = React.createClass ({
-    displayName: 'SearchField',
-    propTypes: {
-        label: React.PropTypes.string,
-    },
-    getInitialState () {
-        return {
-            backspaceRemoves: true,
-            multi: true
-        };
-    },
-    onChange (value) {
-        this.setState({
-            value: value,
-        });
-    },
-    getTags (input) {
-        // if (!input) {
-        //     return Promise.resolve({ options: [] });
-        // }
-        console.log("Input: " + input);
-        return fetch(`/tags?tag=${input}`)
-            .then((response) => {
-                console.log(response.status);
-                return response.json()
-            })
-            .then((json) => {
-                console.log(json);
-                return { options: json};
-            });
-    },
-    render () {
-        return (
-            <div className="section">
-                <Select.Async multi={this.state.multi} value={this.state.value} onChange={this.onChange} valueKey="_id" labelKey="tag" loadOptions={this.getTags} backspaceRemoves={this.state.backspaceRemoves} />
-            </div>
-        );
-    }
+	displayName: 'SearchField',
+	playlists: [],
+	propTypes: {
+		label: React.PropTypes.string,
+	},
+	getInitialState () {
+		return {
+			backspaceRemoves: true,
+			multi: true,
+		};
+	},
+	// When we set a new tag in the search field
+	onChange (value) {
+		this.setState({
+			value: value,
+		});
+		this.getPlaylists(value);
+	},
+
+	// Get playlist info from our backend containing data from the Spotify API
+	getPlaylists(values){
+		// Get values from the input in search field
+		let tags = [];
+		values.forEach(function (value) {
+			tags.push(value.tag)
+		});
+
+		// Fetch playlists from backend with corresponding tags
+		fetch('/playlist/search/' + JSON.stringify({tags:tags}))
+			.then((response) => {
+				return response.json();
+			})
+			.then((json) => {
+				console.log("Result:" + JSON.stringify(json));
+				// Save in variable to be used in render() function
+				playlists = json;
+			})
+	},
+
+	// Fetch tags from backend and print
+	getTags (input) {
+		return fetch(`/tags?tag=${input}`)
+			.then((response) => {
+				return response.json()
+			})
+			.then((json) => {
+				return { options: json};
+			});
+	},
+	render () {
+		// TODO: Fix todo below
+		return (
+			<div className="section">
+				{/* Search field component */}
+				<Select.Async multi={this.state.multi} value={this.state.value} onChange={this.onChange} valueKey="_id" labelKey="tag" loadOptions={this.getTags} backspaceRemoves={this.state.backspaceRemoves} />
+				{/* TODO: Check if this is the right way to display data */}
+				{playlists.map(playlistData => <PlaylistResult key={playlistData.id} {...playlistData} />)}
+			</div>
+		);
+	}
 });
 
 module.exports = SearchField;
