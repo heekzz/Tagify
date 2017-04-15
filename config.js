d91a8cf6133e2e000a81d7558ad3bd961f018f9e
Fredrik Håkansson <freha309@student.liu.se>
2017-04-15 21:27:46 +0200

    Change production adress for Heroku in config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ let config = {};
 
 
 config.port = process.env.PORT || 5000;
-config.spotify_callback = process.env.NODE_ENV==='production' ? "https://tddd27-tagify.herokuapp.com/callback": 'http://localhost:5000/callback';
+config.spotify_callback = process.env.NODE_ENV==='production' ? "https://tddd27tagify.herokuapp.com/callback": 'http://localhost:5000/callback';
 
 module.exports = config;
